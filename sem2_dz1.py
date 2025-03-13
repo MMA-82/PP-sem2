23699fe3f26d664ddcc44dd27bd79e4d30948e3e
Marat <brutto061@gmail.com>
2025-03-13 21:51:12 +0700

    поправили код в дз1

--- sem2_dz1.py
+++ sem2_dz1.py
@@ -6,7 +6,7 @@ base16 = 16
 chars16 = "0123456789abcdef"
 
 n = int(input('Введите число: '))
-print('Оригинал шестнадцатиричный: ',hex(n))
+print('Оригинал шестнадцатиричный: ', hex(n))
 result16 = ''
 
 n_negative = False
@@ -18,8 +18,7 @@ while n > base16:
     result16 += chars16[n % base16]
     n //= base16
 result16 += str(n)
-#result16 = result16 + 'x0'
+# result16 = result16 + 'x0'
 if n_negative:
-        result16 = result16 + '-'
-print('Результат совпадает с оригиналом: ',result16[::-1])
-
+    result16 = result16 + 'x0-'
+print('Результат совпадает с оригиналом: ', result16[::-1])
